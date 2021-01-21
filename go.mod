5f229eaac1dde5eb8d41637e7e561575772d91f9
Andrei Samuilik <a.samuylik@gmail.com>
2021-01-22 00:01:57 +0300

    Project structure refactoring

--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/AgentCoop/heartbeat
-
-go 1.15
-
-require (
-	google.golang.org/grpc v1.35.0 // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
-)
--- src/heartbeat.service/api/server.go
+++ src/heartbeat.service/api/server.go
@@ -1,11 +1,15 @@
-package heartbeat
+package api
 
 import (
 	"fmt"
 	"context"
-	"github.com/AgentCoop/heartbeat/internal/api/heartbeat"
+	"heartbeat.service/internal/api/heartbeat"
 	"google.golang.org/grpc"
 )
+
+func Foo() {
+	fmt.Println("Hello, World!")
+}
 
 type server struct {
 	heartbeat.UnimplementedHeartBeatServer
@@ -14,6 +18,19 @@ type server struct {
 func (s *server) Foo(ctx context.Context, req *heartbeat.Request) (*heartbeat.Response, error) {
 	fmt.Printf("foo request\n")
 	return nil, nil
+}
+
+func NewRequest() *heartbeat.Request {
+	req := &heartbeat.Request{}
+	return req
+}
+
+type Client interface {
+	heartbeat.HeartBeatClient
+}
+//
+func NewClient(conn grpc.ClientConnInterface) Client {
+	return heartbeat.NewHeartBeatClient(conn)
 }
 
 func RegisterServer(s *grpc.Server) {
